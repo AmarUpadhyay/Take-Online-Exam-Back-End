e9d2c18c768e945af4ac1d44a5624ffdc1e12f87
smarthackerp <apuneet71@gmail.com>
2021-01-16 10:17:02 +0530

    Candidate Commmit Successfully...

--- src/main/java/com/capgemini/toe/controller/CandidateController.java
+++ src/main/java/com/capgemini/toe/controller/CandidateController.java
@@ -1,5 +1,24 @@
 package com.capgemini.toe.controller;
 
+import com.capgemini.toe.entity.Test;
+import com.capgemini.toe.service.CandidateService;
+import com.capgemini.toe.service.InstructorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/candidateController")
 public class CandidateController {
+
+    @Autowired
+    private CandidateService candidateService;
+
+
+    @GetMapping("/tests")
+    public ResponseEntity<?> getAllTest(){
+        return new ResponseEntity<>(candidateService.getAllTest(), HttpStatus.OK);
+    }
 
 }
