f2da1cbbc5a82ebf0148586b23345166198e6ee6
smarthackerp <apuneet71@gmail.com>
2021-01-16 10:05:04 +0530

    Candidate Commmit Successfully...

--- src/main/java/com/capgemini/toe/service/CandidateService.java
+++ src/main/java/com/capgemini/toe/service/CandidateService.java
@@ -1,5 +1,16 @@
 package com.capgemini.toe.service;
 
+import com.capgemini.toe.entity.Test;
+import com.capgemini.toe.repository.CandidateTestsRecordRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
 public interface CandidateService {
 
+      public boolean takeTest(long userId, Test test);
+      public double getResult(long userId, Test test);
+      public Test getTestByTestId(long testId);
+      public List<Test> getAllTest();
+      public Test submitTest();
 }
