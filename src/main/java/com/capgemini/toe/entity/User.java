173ee346202cdcae63e87b66a29e3ba431065f82
chnarasi <kasulu9704@gmail.com>
2021-01-17 17:18:15 +0530

    changed table names

--- src/main/java/com/capgemini/toe/entity/User.java
+++ src/main/java/com/capgemini/toe/entity/User.java
@@ -4,10 +4,12 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotNull;
 
 @Entity
+@Table(name="User_Data")
 public class User {
 	
 	@Id
